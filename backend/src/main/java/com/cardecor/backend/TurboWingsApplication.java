a376d461345ee5c04f8107b16ac92dc049ee2916
Tamilarasan M <tamilarasan.muthusamy@knowledge-values.com>
2025-01-22 21:14:25 +0100

    Initial Spring Boot setup

---
+++ backend/src/main/java/com/cardecor/backend/TurboWingsApplication.java
@@ -0,0 +1,13 @@
+package com.cardecor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboWingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboWingsApplication.class, args);
+	}
+
+}
